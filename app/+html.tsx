71c1a85aa14ec00dc40be16bd844eb67b953b7cc
acipuns-dot <acipuns@gmail.com>
2026-07-07 14:00:53 +0800

    feat: add PWA web manifest and +html configurations

---
+++ app/+html.tsx
@@ -0,0 +1,28 @@
+import { ScrollViewStyleReset } from 'expo-router/html';
+import React from 'react';
+
+export default function Root({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+        {/* PWA Manifest Link */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#1C1340" />
+
+        <ScrollViewStyleReset />
+        <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
+
+const responsiveBackground = `
+  body {
+    background-color: #F8FAFC;
+  }
+`;
